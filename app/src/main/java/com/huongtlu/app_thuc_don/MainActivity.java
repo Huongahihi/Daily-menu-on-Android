6aae6fbb4f40d27c09245686fec0f40707be495d
Vũ Thị Hường <huongvt201972gmail.com>
2018-03-23 22:41:12 +0700

    Hoan thanh ruc ro

--- app/src/main/java/com/huongtlu/app_thuc_don/MainActivity.java
+++ app/src/main/java/com/huongtlu/app_thuc_don/MainActivity.java
@@ -1,14 +1,19 @@
 package com.huongtlu.app_thuc_don;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    Button btn_cb,btn_tp,btn_yt,btn_tt,btn_ls,btn_qn,btn_cn;
+    Button btn_cb, btn_tp, btn_yt, btn_tt, btn_ls, btn_qn, btn_cn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,54 +23,61 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btn_tp.setOnClickListener(this);
         btn_qn.setOnClickListener(this);
         btn_cn.setOnClickListener(this);
+        btn_ls.setOnClickListener(this);
+        btn_yt.setOnClickListener(this);
     }
     private void Anhxa() {
-        btn_cb=(Button) findViewById(R.id.btnChebien);
-        btn_tp=(Button) findViewById(R.id.btnThucpham);
-        btn_yt=(Button) findViewById(R.id.btnBuaan);
-        btn_tt=(Button) findViewById(R.id.btnTintuc);
-        btn_ls=(Button) findViewById(R.id.btnLichSu);
-        btn_qn=(Button) findViewById(R.id.btnQuanan);
-        btn_cn=(Button) findViewById(R.id.btnCapnhat);
+        btn_cb = (Button) findViewById(R.id.btnChebien);
+        btn_tp = (Button) findViewById(R.id.btnThucpham);
+        btn_yt = (Button) findViewById(R.id.btnBuaan);
+        btn_tt = (Button) findViewById(R.id.btnTintuc);
+        btn_ls = (Button) findViewById(R.id.btnLichSu);
+        btn_qn = (Button) findViewById(R.id.btnQuanan);
+        btn_cn = (Button) findViewById(R.id.btnCapnhat);
 
     }
+
     @Override
     public void onClick(View view) {
-        int id=view.getId();
-        if(id==R.id.btnChebien){
+        int id = view.getId();
+        if (id == R.id.btnChebien) {
             //Menu tìm kiếm: theo Bữa sáng, trức, tối.
-            Intent intent=new Intent(this,ThucdonActivity.class);
+            Intent intent = new Intent(this, ThucdonActivity.class);
             startActivity(intent);
         }
-        if(id==R.id.btnThucpham){
+        if (id == R.id.btnThucpham) {
             //Gọi tới activi list view tương tự như chế biến.
-            Intent intent=new Intent(this,ThucdonActivity.class);
+            Intent intent = new Intent(this, MonanDDActivity.class);
             startActivity(intent);
         }
-        if(id==R.id.btnBuaan){
+        if (id == R.id.btnBuaan) {
             //Gọi tới hàm bữa ăn yêu thích.
+            Toast.makeText(this,"Bữa ăn yêu thích trống",Toast.LENGTH_SHORT).show();
         }
-        if(id==R.id.btnTintuc){
+        if (id == R.id.btnTintuc) {
             //Gọi tơi 1 activity tin tức: Đó là load trang Vnexpress đã làm( về chủ để sức khỏe. Phải có kết nói internet
-            Intent intent=new Intent(Intent.ACTION_VIEW,Uri.parse("https://suckhoe.vnexpress.net/"));
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://suckhoe.vnexpress.net/"));
             startActivity(intent);
         }
-        if(id==R.id.btnLichSu){
-            Lichsu();
-            //Gọi tới hàm lịch sư
+        if (id == R.id.btnLichSu) {
+            doCalling();
         }
-        if(id==R.id.btnQuanan){
+        if (id == R.id.btnQuanan) {
             //Call to quanngon on internet.
-            Intent i=new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.foody.vn/"));
+            Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.foody.vn/"));
             startActivity(i);
         }
-        if(id==R.id.btnCapnhat){
+        if (id == R.id.btnCapnhat) {
             //Gọi tới hàm cập nhật
-            Intent intent=new Intent(this,Update_Activity.class);
+            Intent intent = new Intent(this, Update_Activity.class);
             startActivity(intent);
         }
     }
-    private void Lichsu() {
 
+    @SuppressLint("MissingPermission")
+    private void doCalling() {
+        Intent intent = new Intent(Intent.ACTION_CALL);
+        intent.setData(Uri.parse("tel:0971865431"));
+        startActivity(intent);
     }
 }
